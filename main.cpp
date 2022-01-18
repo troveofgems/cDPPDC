4fd3a87b7b8e242bd9f86d0b9b4e878f803b7e5a
Dustin Kent Greco <dkgreco@thetroveofgems.tech>
2022-01-17 20:19:17 -0700

    Initial Commit For Project

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+/* *
+ * Section 2: Fundamentals
+ *
+ * Objectives
+ * ----------
+ * * * * * * */
+ // * - Variables & Data Types
+ int runVariablesModule() {
+     cout << "Running THe Variables Module..." << endl;
+
+     return 0;
+ }
+ // * - Comments
+ int comments() {
+     return 0;
+ }
+ // * - Arithmetic Operators
+ int arithmeticOperators() {
+     return 0;
+ }
+ // * - Relational Operators
+ int relationalOperators() {
+     return 0;
+ }
+ // * - Logical Operators
+ int logicalOperators() {
+     return 0;
+ }
+ // * - Symbolic Constants and Naming Conventions
+ int symbolicConstants() {
+     return 0;
+ }
+ // * - Keyboard Input
+ int keyboardInput() {
+     return 0;
+ }
+
+/* *
+* Section 2: Fundamentals
+*
+* Projects
+* ---------
+* * * * * */
+ // * - Project Average Of Three
+ int averageOfThree() {
+     return 0;
+ }
+ // * - Project Madlibs Clone
+ int madLibs() {
+    return 0;
+ }
+
+
+int main() {
+    cout << "Hello, Dustin! Running Section 2 C++ Modules!" << endl;
+    runVariablesModule();
+    return 0;
+}
